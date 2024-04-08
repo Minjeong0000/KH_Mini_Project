9476c418bc016a3052eff302004923444f18c591
DESKTOP-C6U7CCJ\user1 <seongi0407@naver.com>
2024-04-08 09:50:08 +0900

    Connector C##KH로 변경

--- javaCode/miniPrj/src/util/JDBCTemplate.java
+++ javaCode/miniPrj/src/util/JDBCTemplate.java
@@ -11,7 +11,7 @@ public class JDBCTemplate {
 		String driver = "oracle.jdbc.OracleDriver";
 		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
 		// 계정에 따라 id 값 바꿔서 하기
-		String id = "C##MINI";
+		String id = "C##KH";
 		String pwd = "1234";
 		Class.forName(driver);
 		Connection conn = DriverManager.getConnection(url, id, pwd);
